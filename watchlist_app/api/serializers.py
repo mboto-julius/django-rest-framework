7501a96b55bb9f4910182c1bb1360fde29ded932
Julius Mboto <juliusmboto@gmail.com>
2026-06-02 11:05:57 +0300

    feat: model serializer

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,33 +1,23 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie 
 
-def name_length(value):
-    if len(value) < 2:
-        raise serializers.ValidationError('Name must be at least 2 characters long')
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[name_length])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
-    
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
-    
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+        # fields = ['id', 'name', 'description']
+        # exclude = ['active']
+        
     def validate(self, data):
         if data['name'] == data['description']:
             raise serializers.ValidationError('Name and description must be different')
         return data
     
-    # def validate_name(self, value):
-    #     value = value.strip()
-    #     if len(value) < 2:
-    #         raise serializers.ValidationError('Name must be at least 2 characters long')
-    #     return value
+    def validate_name(self, value):
+        value = value.strip()
+        if len(value) < 2:
+            raise serializers.ValidationError('Name must be at least 2 characters long')
+        return value
+
+    
