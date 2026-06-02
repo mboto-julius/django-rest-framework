22a7db3f9951132ec3838e5c52e8e0a3b163ae42
Julius Mboto <juliusmboto@gmail.com>
2026-06-02 10:30:57 +0300

    feat: add serializer field and custom validation

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie 
 
+def name_length(value):
+    if len(value) < 2:
+        raise serializers.ValidationError('Name must be at least 2 characters long')
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
     
@@ -16,3 +20,14 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+    
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError('Name and description must be different')
+        return data
+    
+    # def validate_name(self, value):
+    #     value = value.strip()
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError('Name must be at least 2 characters long')
+    #     return value
