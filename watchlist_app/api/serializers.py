de2f8e8c3be076e9f9cb00e55ae53df24e93dc02
Julius Mboto <juliusmboto@gmail.com>
2026-06-02 11:44:04 +0300

    feat: custom serializer field

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -3,11 +3,16 @@ from watchlist_app.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    len_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Movie
         fields = '__all__'
         # fields = ['id', 'name', 'description']
         # exclude = ['active']
+        
+    def get_len_name(self, object):
+        return len(object.name)
         
     def validate(self, data):
         if data['name'] == data['description']:
